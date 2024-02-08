12ed9450677cffc94379a0660347806518319d82
Aditya Chauhan <aditychauhan84523@gmail.com>
2024-02-08 23:26:32 +0530

    trying to fix cors issue

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,27 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
+    port:3000,
     // for production
     proxy: {
       '/api': {
-        target: 'https://bucketbuybackend.onrender.com',
+        target: 'https://bucketbuy.onrender.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
-    },
+        secure: false,      
+        ws: true,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      }
+    }
 
     // for testing
     // proxy:{
