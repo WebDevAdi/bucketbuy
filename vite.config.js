1ee4641597678c1c0d3e270e94df532e86a8df04
Aditya Chauhan <aditychauhan84523@gmail.com>
2024-02-12 14:10:46 +0530

    changed vite.config.js server configuration to solve proxy issues

--- vite.config.js
+++ vite.config.js
@@ -5,31 +5,20 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    port:3000,
-    // for production
-    proxy: {
-      '/api': {
-        target: 'https://bucketbuy.onrender.com',
-        changeOrigin: true,
-        secure: false,      
-        ws: true,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      }
-    }
-
     // for testing
     // proxy:{
     //   '/api':'http://localhost:3000'
-    // }
+    // },
+
+    // for production
+    proxy: {
+      '/api': {
+        target: 'https://bucketbuybackend.onrender.com', // Change this to your production API endpoint
+        changeOrigin: true,
+        secure:false,
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
+      },
+    },
+
   }
 })
